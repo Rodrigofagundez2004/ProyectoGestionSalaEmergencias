8f23bf653368ab4da28a11fab9ad9e2c905837de
RodriGarcia12 <rgarciaa125@gmail.com>
2026-08-22 13:45:29 -0300

    metodo actualizarUrgencia

--- src/main/java/ucu/edu/aed/SalaDeEmergencias/SalaDeEspera.java
+++ src/main/java/ucu/edu/aed/SalaDeEmergencias/SalaDeEspera.java
@@ -4,7 +4,13 @@ import ucu.edu.aed.implementaciones.ColaConPrioridad;
 import java.util.Comparator;
 
 public class SalaDeEspera {
+    private static final int LIMITE_ESPERA_URGENCIA_BAJA = 4;
+    private static final int LIMITE_ESPERA_URGENCIA_MEDIA = 3;
+    private static final int LIMITE_ESPERA_URGENCIA_ALTA = 2;
+    private static final int LIMITE_ESPERA_URGENCIA_URGENTE = 1;
+
     private ColaConPrioridad<Paciente> porAtender;
+    
     
     public SalaDeEspera(){
         porAtender = new ColaConPrioridad<Paciente>(new ComparadorPacientes());
@@ -21,18 +27,37 @@ public class SalaDeEspera {
         for (int i = 0; i < porAtender.tamaño(); i++) {
             Paciente paciente = porAtender.obtener(i);
             paciente.setTiempoEsperando(paciente.getTiempoEsperando()+1);
+            actualizarUrgencia(paciente);
         }
+        
     }
     
-    public boolean actualizarUrgencia(){
+    private boolean actualizarUrgencia(Paciente paciente){
         /*
         Verifica si el paciente superó el tiempo de espera establecido para 
         aumentar su nivel de prioridad. Si corresponde, modifica su nivel de 
         urgencia y actualiza su posición dentro de la ColaPrioridad.
-        Devuelve true si la prioridad pudo actualizarse y false si no se pudo.
+        Devuelve valoresActualizados, si la prioridad pudo actualizarse la variable sera true y false si no se pudo.
          */
-        return false;
+        boolean valoresActualizados = false;
+
+        if (paciente.getPrioridad() == NivelPrioridad.BAJA && paciente.getTiempoEsperando() == LIMITE_ESPERA_URGENCIA_BAJA) {
+            paciente.setPrioridad(NivelPrioridad.MEDIA);
+            paciente.setTiempoEsperando(0);
+            valoresActualizados = true;
+        } else if (paciente.getPrioridad() == NivelPrioridad.MEDIA && paciente.getTiempoEsperando() == LIMITE_ESPERA_URGENCIA_MEDIA) {
+            paciente.setPrioridad(NivelPrioridad.ALTA);
+            paciente.setTiempoEsperando(0);
+            valoresActualizados = true;
+        } else if (paciente.getPrioridad() == NivelPrioridad.ALTA && paciente.getTiempoEsperando() == LIMITE_ESPERA_URGENCIA_ALTA) {
+            paciente.setPrioridad(NivelPrioridad.URGENTE);
+            paciente.setTiempoEsperando(0);
+            valoresActualizados = true;
+        }
+
+        return valoresActualizados;
     }
+    
     public ColaConPrioridad<Paciente> getPorAtender() {
         return porAtender;
     }
